9ff166c2c51cedcc96172559c1c89956df580703
GuardianAngel2138 <96695654+GuardianAngel2138@users.noreply.github.com>
2024-10-15 13:10:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 BOT_TOKEN = "7454473347:AAFgeNX6pOelmQTgeDEV0NZZlHhGwAIw9U4"
 MONGODB_URI = "mongodb+srv://yuuvivienne31:yuuvivienne31@cluster0.4v2dc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 OWNER_CHAT_ID = "5277330082"
-CHAT_ID = '-1001876110191'
+CHAT_ID = '-1002091739332'
