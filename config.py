0bf8df43f4bfc8900ba7329b26b0749b00488432
GuardianAngel2138 <96695654+GuardianAngel2138@users.noreply.github.com>
2024-10-16 23:41:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 # config.py
 BOT_TOKEN = "7454473347:AAFgeNX6pOelmQTgeDEV0NZZlHhGwAIw9U4"
-MONGODB_URI = "mongodb+srv://yuuvivienne31:yuuvivienne31@cluster0.4v2dc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-OWNER_CHAT_ID = "5277330082"
-CHAT_ID = '-1002091739332'
+GROUP_ID = "-1002004450289"
+USER_ID = "5277330082"  # Add this line
+MONGO_URI = "mongodb+srv://abhinavkrishna101:abhinavkrishna101@cluster0.agbobsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+TMDB_API_KEY = "971a28dca9fc7c1747ffd24aeef1d413"
+CHECK_INTERVAL = 3660  # Set the interval as needed
+RANDOM_DELAY_IN = (5, 10)  # Random delay between 5 and 10 seconds
+RANDOM_DELAY_ANY = (3, 8)   # Random delay between 3 and 8 seconds
+RANDOM_DELAY_NEW = (6, 12)   # Random delay between 6 and 12 seconds
